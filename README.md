e25070b8f4055f436d947aa1c8738977880fe4b6
Kenway28 <lotnar33@gmail.com>
2022-04-23 21:41:13 +0100

    Add Some modifications

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 ## WordBreaker
 
+### update 1.2.1
+
+- Add Local Storage Option
+
 ### update 1.2
+
 - Add Some Settings
 - Add level System from 4 to 7
 - Add Dark mode
 
-### update 1.1 
+### update 1.1
+
 - Update Input System
-- Add Character Map 
-- Update css File 
-
-
+- Add Character Map
+- Update css File
--- Word Breaker.js
+++ Word Breaker.js
@@ -2,54 +2,88 @@ letterBtn = document.querySelectorAll(".row-button:not(.clear, .enter)");
 btnEnter = document.querySelector(".enter");
 btnClear = document.querySelector(".clear");
 game = document.querySelector(".game");
-
 let levels = document.querySelectorAll(".level-list li");
-let Vlist = "last games hospital visited accept";
+
+window.onload = function () {
+  let Mode = JSON.parse(localStorage.getItem("WordLe")).mode;
+  let Level = JSON.parse(localStorage.getItem("WordLe")).level;
+  if (Mode === "dark") {
+    toggle.classList.toggle("active");
+    document.body.classList.toggle("dark");
+  }
+  if (Level != "") {
+ 
+    levels.forEach((lvl) => {
+      lvl.classList.remove("level");
+      if (lvl.innerHTML == Level) {
+        lvl.classList.add("level");
+        generateGame(+Level);
+      }
+    });
+  } else {
+    generateGame(4);
+  }
+};
+/***************** Settings */
+let settings = {
+  mode: "",
+  level: "",
+};
+let toggle = document.getElementById("toggle");
+let indicator = document.querySelector(".indicator");
+toggle.onclick = function () {
+  document.body.classList.toggle("dark");
+  toggle.classList.toggle("active");
+  settings.mode = document.body.className;
+  saveSettings(settings);
+};
+
+function saveSettings(settings) {
+  localStorage.setItem("WordLe", JSON.stringify(settings));
+}
+let WordList = "last gone games hospital visited accept reload";
 let theWord;
+let words;
 levels.forEach((lvl) => {
   lvl.addEventListener("click", () => {
     levels.forEach((lv) => {
       lv.classList.remove("level");
     });
     lvl.classList.add("level");
+    settings.level = lvl.innerHTML;
+    saveSettings(settings);
+    console.log(settings);
     generateGame(lvl.innerHTML);
   });
 });
 function generateGame(size) {
-  theWord = Vlist.split(" ")
-    .filter((e) => {
-      if (e.length == size) {
-        return e;
-      }
-    })
-    .join("");
+  words = WordList.split(" ").filter((e) => {
+    if (e.length == size) {
+      return e;
+    }
+  });
+  theWord = words[Math.floor(Math.random() * words.length)];
+  let wordIndex = words.indexOf(theWord);
+  words.splice(wordIndex, 1);
   console.log(theWord);
   game.innerHTML = "";
 
   for (let i = 0; i < 7; i++) {
     let line = document.createElement("div");
+    line.className = "line";
     for (let j = 0; j < size; j++) {
       let letter = document.createElement("span");
       letter.className = "letter";
-      line.className = "line";
       line.appendChild(letter);
     }
     game.appendChild(line);
   }
 }
-window.onload = () => {
-  generateGame(4);
-};
-
 
 document.body.addEventListener("keydown", (event) => {
   let row = game.querySelector(".line:not(.locked)");
 
-  if (
-    event.key.match(/[a-z]/) &&
-    event.key != "Enter" &&
-    event.key != "Backspace"
-  ) {
+  if (event.key.match(/[a-z]/) && event.key.length == 1) {
     for (let i = 0; i < Array.from(row.children).length; i++) {
       if (Array.from(row.children)[i].innerHTML == "") {
         Array.from(row.children)[i].innerHTML = event.key;
@@ -82,6 +116,7 @@ document.body.addEventListener("keydown", (event) => {
         } else {
           box.className = "letter noMatch";
         }
+
         letterBtn.forEach((letter) => {
           if (letter.innerHTML == box.innerHTML) {
             letter.className = `row-button ${box.className.split(" ")[1]}`;
@@ -89,5 +124,12 @@ document.body.addEventListener("keydown", (event) => {
         });
       });
     }
+    if (row.querySelectorAll(".letter.match").length == theWord.length) {
+      setTimeout(() => {
+        game.innerHTML = "finished";
+        // generateGame(document.querySelector(".level").innerHTML);
+      }, 2000);
+    } 
+    console.log(row.querySelectorAll(".letter.match").length);
   }
 });
